35b8cdd87b2386fc3d915ee23b536b6e7b81037d
eidytrinidad <eidyjtf@gmail.com>
2020-09-25 17:00:08 -0400

    uploading custom hooks

---
+++ useFetch.jsx
@@ -0,0 +1,48 @@
+import { useRef } from "react";
+import { useEffect, useState } from "react";
+
+export const useFetch = (url) => {
+
+  const isMounted = useRef(true)
+
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+
+  useEffect(() => {
+    
+    return () => {
+      isMounted.current=false
+    }
+  }, [])
+
+  useEffect(() => {
+
+    setState({
+        data:null,
+        loading:true,
+        error:null
+    })
+
+    fetch(url)
+            .then(response=>response.json())
+            .then(data=>{
+               
+                if (isMounted.current) {
+                  setState({
+                    loading:false,
+                    error:null,
+                    data
+                    })
+                
+                }
+
+            })
+
+  }, [url]);
+
+  return state;
+};
